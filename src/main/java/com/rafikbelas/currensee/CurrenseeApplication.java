c8dd033122f9bf2845cbb4bc69949a0546383b5c
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-20 08:54:49 +0100

    Initial commit.
    Set up basic project pom.

---
+++ src/main/java/com/rafikbelas/currensee/CurrenseeApplication.java
@@ -0,0 +1,13 @@
+package com.rafikbelas.currensee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenseeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrenseeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafikbelas/currensee/CurrenseeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafikbelas.currensee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrenseeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
