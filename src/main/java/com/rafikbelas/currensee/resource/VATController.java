368695ef7de4a55e98d45ef8fca4b8cf4ae0998e
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-21 18:48:07 +0100

    Linked verify VAT method to CloudMersive API.

--- src/main/java/com/rafikbelas/currensee/resource/VATController.java
+++ src/main/java/com/rafikbelas/currensee/resource/VATController.java
@@ -1,5 +1,12 @@
 package com.rafikbelas.currensee.resource;
 
+import com.cloudmersive.client.VatApi;
+import com.cloudmersive.client.invoker.ApiClient;
+import com.cloudmersive.client.invoker.ApiException;
+import com.cloudmersive.client.invoker.Configuration;
+import com.cloudmersive.client.invoker.auth.ApiKeyAuth;
+import com.cloudmersive.client.model.VatLookupRequest;
+import com.cloudmersive.client.model.VatLookupResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class VATController {
 
     @GetMapping("/verify")
-    String verify(@RequestParam("VAT") String vat) {
-        return "Verifying VAT " + vat;
+    VatLookupResponse verify(@RequestParam("VAT") String vat,
+                             @RequestParam("api_key") String apiKey) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+
+        ApiKeyAuth Apikey = (ApiKeyAuth) defaultClient.getAuthentication("Apikey");
+        Apikey.setApiKey(apiKey);
+
+        VatApi apiInstance = new VatApi();
+        VatLookupRequest input = new VatLookupRequest().vatCode(vat);
+
+        try {
+            VatLookupResponse result = apiInstance.vatVatLookup(input);
+            return result;
+        } catch (ApiException e) {
+            System.err.println("Exception when calling VatApi#vatVatLookup");
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
