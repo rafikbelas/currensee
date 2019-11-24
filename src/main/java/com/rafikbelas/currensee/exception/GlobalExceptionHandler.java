513b8f0bd4cb95ab4f3c242210bfc462d1c7d089
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-24 05:27:54 +0100

    Overriden handle MissingServletrequestParameter exception in Exception Handler.

--- src/main/java/com/rafikbelas/currensee/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rafikbelas/currensee/exception/GlobalExceptionHandler.java
@@ -1,25 +1,49 @@
 package com.rafikbelas.currensee.exception;
 
 import com.cloudmersive.client.invoker.ApiException;
+import io.swagger.annotations.Api;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
-
 import java.net.UnknownHostException;
 
 import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private static final String UNEXPECTED_ERROR = "Exception.unexpected";
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+                                                                          HttpHeaders headers,
+                                                                          HttpStatus status,
+                                                                          WebRequest request) {
+
+        ApiError apiError = new ApiError(BAD_REQUEST, ex.getLocalizedMessage());
+        return new ResponseEntity<>(apiError, BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ApiError handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String message = String.format("The parameter '%s' of value '%s' could not be converted to type '%s'",
+                ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+        return new ApiError(BAD_REQUEST, message, ex);
+    }
 
     @ExceptionHandler(InvalidVatException.class)
     @ResponseStatus(NOT_FOUND)
     public ApiError handeInvalidVatException(InvalidVatException ex) {
-        String message = String.format("The parameter VAT is not a valid VAT number.");
+        String message = "The parameter 'VAT' is not a valid VAT number.";
         return new ApiError(NOT_FOUND, message, ex);
     }
 
@@ -29,14 +53,6 @@ public class GlobalExceptionHandler {
         ApiError apiError = new ApiError(BAD_REQUEST, "Validation error");
         apiError.addValidationErrors(ex.getConstraintViolations());
         return apiError;
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    @ResponseStatus(BAD_REQUEST)
-    public ApiError handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
-        String message = String.format("The parameter '%s' of value '%s' could not be converted to type '%s'",
-                ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
-        return new ApiError(BAD_REQUEST, message, ex);
     }
 
     @ExceptionHandler(ApiException.class)
