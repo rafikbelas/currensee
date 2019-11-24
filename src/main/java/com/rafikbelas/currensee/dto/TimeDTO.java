1d157a9844a7da42f69263c0cbdb03782690bbd0
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-24 23:49:37 +0100

    Removed MapStrcut.
    Edited TimeController to return new object instead of going through MapStruct for DTO conversion.

--- src/main/java/com/rafikbelas/currensee/dto/TimeDTO.java
+++ src/main/java/com/rafikbelas/currensee/dto/TimeDTO.java
@@ -1,12 +1,12 @@
 package com.rafikbelas.currensee.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.time.LocalTime;
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
 public class TimeDTO {
 
     private LocalTime now;
--- src/main/java/com/rafikbelas/currensee/resource/TimeController.java
+++ src/main/java/com/rafikbelas/currensee/resource/TimeController.java
@@ -1,8 +1,6 @@
 package com.rafikbelas.currensee.resource;
 
 import com.rafikbelas.currensee.dto.TimeDTO;
-import com.rafikbelas.currensee.mapper.TimeMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +9,8 @@ import java.time.LocalTime;
 @RestController
 public class TimeController {
 
-    @Autowired
-    private TimeMapper timeMapper;
-
     @GetMapping("/now")
     TimeDTO now() {
-        return timeMapper.toTimeDTO(LocalTime.now());
+        return new TimeDTO(LocalTime.now());
     }
 }
