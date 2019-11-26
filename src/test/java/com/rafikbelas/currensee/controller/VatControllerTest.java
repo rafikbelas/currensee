4b97b3296bf8b39fc0240a0a8c116991678d3383
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-26 09:59:22 +0100

    Added test case for VatController.convert in case VAT is invalid.

--- src/test/java/com/rafikbelas/currensee/controller/VatControllerTest.java
+++ src/test/java/com/rafikbelas/currensee/controller/VatControllerTest.java
@@ -1,5 +1,6 @@
 package com.rafikbelas.currensee.controller;
 
+import com.rafikbelas.currensee.exception.InvalidVatException;
 import com.rafikbelas.currensee.service.VatService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -47,5 +49,18 @@ class VatControllerTest {
     void verify_shouldReturnBadRequestIfParamIsMissing() throws Exception {
         mockMvc.perform(get(urlTemplate))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void convert_returnsBadRequestIfVatIsInvalid() throws Exception {
+        vat = "AA123456";
+        doThrow(InvalidVatException.class).when(vatService).getCountryCode(vat, apiKey);
+
+        mockMvc.perform(get(urlTemplate)
+                .param("vat", vat)
+                .param("api_key", apiKey))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
     }
 }
