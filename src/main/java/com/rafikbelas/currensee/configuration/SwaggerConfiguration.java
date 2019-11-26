3ffac3fc1cd6bdfdfeaecac3048a4b84f8b55720
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-27 00:22:17 +0100

    Added SwaggerConfiguration file and enabled Swagger documentation.

---
+++ src/main/java/com/rafikbelas/currensee/configuration/SwaggerConfiguration.java
@@ -0,0 +1,9 @@
+package com.rafikbelas.currensee.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+}
