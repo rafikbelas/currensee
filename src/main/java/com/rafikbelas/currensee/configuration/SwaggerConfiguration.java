9f4ff4e33d595c09b6ed7feed92b56a7924d0e2b
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-27 00:34:35 +0100

    Added Docket bean to customize Swagger documentation.

--- src/main/java/com/rafikbelas/currensee/configuration/SwaggerConfiguration.java
+++ src/main/java/com/rafikbelas/currensee/configuration/SwaggerConfiguration.java
@@ -1,9 +1,51 @@
 package com.rafikbelas.currensee.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.time.LocalTime;
+
 @Configuration
+@PropertySource("classpath:swagger.properties")
 @EnableSwagger2
 public class SwaggerConfiguration {
+
+    @Value("${documentation.title}")
+    String title;
+
+    @Value("${documentation.description}")
+    String description;
+
+    @Value("${documentation.version}")
+    String version;
+
+    @Value("${documentation.base-package}")
+    String basePackage;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(basePackage))
+                .build()
+                .directModelSubstitute(LocalTime.class, String.class)
+                .apiInfo(apiInfo());
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(title)
+                .description(description)
+                .version(version)
+                .build();
+    }
 }
