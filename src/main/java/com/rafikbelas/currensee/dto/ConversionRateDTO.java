07c6f5f5d0b00a4697dbf4d24c809551d73dcf88
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-27 12:45:12 +0100

    Added Swagger documentation to ConversionRateDTO.

--- src/main/java/com/rafikbelas/currensee/dto/ConversionRateDTO.java
+++ src/main/java/com/rafikbelas/currensee/dto/ConversionRateDTO.java
@@ -1,15 +1,22 @@
 package com.rafikbelas.currensee.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ApiModel(value = "ConversionResult", description = "The result of the conversion from USD to target currency")
 public class ConversionRateDTO {
 
+    @ApiModelProperty(notes = "${ConversionRateDTO.from}")
     private String from;
+    @ApiModelProperty(notes = "${ConversionRateDTO.to}")
     private String to;
+    @ApiModelProperty(notes = "${ConversionRateDTO.amount}")
     @JsonProperty("amount")
     private double convertedAmount;
 
