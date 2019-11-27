e347ebdf40ff59721a5f03a3d24f08bd6b233b74
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-27 13:50:28 +0100

    Added Swagger Docs to VatResultDTO.

--- src/main/java/com/rafikbelas/currensee/dto/VatResultDTO.java
+++ src/main/java/com/rafikbelas/currensee/dto/VatResultDTO.java
@@ -1,5 +1,8 @@
 package com.rafikbelas.currensee.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +10,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@ApiModel(value = "VatResult", description = "Result of verification.")
 public class VatResultDTO {
+    @ApiModelProperty(notes = "${VatResultDTO.countryCode}")
     private String countryCode;
 }
