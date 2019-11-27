115ecd7bb0957782d8c5855b54c0a13be74449d3
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-27 13:17:54 +0100

    Added Swagger docs to TimeDTO.

--- src/main/java/com/rafikbelas/currensee/dto/TimeDTO.java
+++ src/main/java/com/rafikbelas/currensee/dto/TimeDTO.java
@@ -1,5 +1,7 @@
 package com.rafikbelas.currensee.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,7 +9,8 @@ import java.time.LocalTime;
 
 @Data
 @AllArgsConstructor
+@ApiModel(value = "Time", description = "The current time")
 public class TimeDTO {
-
+    @ApiModelProperty(notes = "${TimeDTO.now}")
     private LocalTime now;
 }
