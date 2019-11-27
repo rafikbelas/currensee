b40015d28eca52cea2b2f3e0acadb9fd9c70ba45
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-27 12:22:16 +0100

    Added Swagger documentation to CurrencyController.

--- src/main/java/com/rafikbelas/currensee/controller/CurrencyController.java
+++ src/main/java/com/rafikbelas/currensee/controller/CurrencyController.java
@@ -3,6 +3,9 @@ package com.rafikbelas.currensee.controller;
 import com.rafikbelas.currensee.dto.ConversionRateDTO;
 import com.rafikbelas.currensee.service.CurrencyService;
 import com.rafikbelas.currensee.validator.CurrencyCodeConstraint;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/currency")
 @Validated
+@Api(tags = "Currency", description = "REST API for Currency")
 public class CurrencyController {
     public static final String FROM = "USD";
 
     @Autowired
     private CurrencyService currencyService;
 
+    @ApiOperation(value = "${CurrencyController.convert.description}", response = ConversionRateDTO.class)
     @GetMapping("/convert")
-    ConversionRateDTO convert(@RequestParam("to") @CurrencyCodeConstraint String to,
-                              @RequestParam("amount") double amount,
+    ConversionRateDTO convert(@ApiParam(value = "${CurrencyController.convert.to}", required = true, example = "EUR")
+                              @RequestParam @CurrencyCodeConstraint String to,
+                              @ApiParam(value = "${CurrencyController.convert.amount}", required = true, example = "100.50")
+                              @RequestParam double amount,
+                              @ApiParam(value = "${CurrencyController.convert.apiKey}", required = true)
                               @RequestParam("api_key") String apiKey) {
 
         double rate = currencyService.getRate(FROM, to, apiKey);
