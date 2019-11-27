0b5d67b4e36ab0c70264b134993570f8ac1dcb9c
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-27 13:41:02 +0100

    Added Swagger docs to VatController.

--- src/main/java/com/rafikbelas/currensee/controller/VatController.java
+++ src/main/java/com/rafikbelas/currensee/controller/VatController.java
@@ -1,8 +1,10 @@
 package com.rafikbelas.currensee.controller;
 
-import com.cloudmersive.client.invoker.ApiException;
 import com.rafikbelas.currensee.dto.VatResultDTO;
 import com.rafikbelas.currensee.service.VatService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/vat")
+@Api(tags = "VAT", description = "REST API for VAT")
 public class VatController {
 
     @Autowired
     VatService vatService;
 
     @GetMapping("/verify")
-    VatResultDTO verify(@RequestParam("vat") String vat,
+    @ApiOperation(value = "${VatController.verify.description}", response = VatResultDTO.class)
+    VatResultDTO verify(@ApiParam(value = "${VatController.verify.vat.description}", required = true, example = "CZ25123891")
+                        @RequestParam("vat") String vat,
+                        @ApiParam(value = "${VatController.verify.apiKey.description}", required = true)
                         @RequestParam("api_key") String apiKey) {
 
         String countryCode = vatService.getCountryCode(vat, apiKey);
