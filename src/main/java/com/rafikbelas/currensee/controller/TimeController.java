fe518a2473b660beca2e81c01e8cabd31415e2db
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-27 13:09:20 +0100

    Added Swagger doc for TimeController.

--- src/main/java/com/rafikbelas/currensee/controller/TimeController.java
+++ src/main/java/com/rafikbelas/currensee/controller/TimeController.java
@@ -1,14 +1,18 @@
 package com.rafikbelas.currensee.controller;
 
 import com.rafikbelas.currensee.dto.TimeDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalTime;
 
 @RestController
+@Api(tags = "Time", description = "REST API for Time")
 public class TimeController {
 
+    @ApiOperation(value = "${TimeController.now.description}", response = TimeDTO.class)
     @GetMapping("/now")
     TimeDTO now() {
         return new TimeDTO(LocalTime.now());
